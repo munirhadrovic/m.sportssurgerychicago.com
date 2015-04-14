96c491ce405127eb0b1bf3b361a41c433e9a1dd4
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-14 15:20:12 +0200

    Dodao "rel="external"" na link (footer).

--- first/index.php
+++ first/index.php
@@ -93,7 +93,7 @@
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
 
-            <h1 style="color: black;">Powered by <a href="http://mobilegrows.com/">MobileGrows </a>|<a href="http://www.sportssurgerychicago.com/" rel="external"> DesktopSite</a></h1>
+            <h1 style="color: black;">Powered by <a href="http://mobilegrows.com/" rel="external">MobileGrows </a>|<a href="http://www.sportssurgerychicago.com/" rel="external"> DesktopSite</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
